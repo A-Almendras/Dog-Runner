2243be4d01fff7f6faf95f230b9988ad138990b6
Anaflavia Almendras <anaflaviaalmendras@gmail.com>
2022-07-04 13:11:41 -0400

    Dog item jumps when click event occurs

--- script.js
+++ script.js
@@ -3,14 +3,26 @@
 const dog = document.querySelector('#dog')
 const obstacle = document.querySelector('#fire-hydrant')
 const bone = document.querySelector('#bone')
+let gameOver = false
 
 ////////////////////////////////
 // Functions For Game Logic Here
 
-// To allow dog to jump
+// To allow dog to jump when click event occurs
 const jump = () => {
+  let count = 0
+
+  // animation will occur in a class called jump. When user clicks want to activate jump animation by adding the class
   dog.classList.add('jump')
   console.log('jump')
+  count++
+
+  // based on keyframes in CSS animation lasts for .5 secs, so want to remove the class after .5 secs so we can redo animation whenever we click again
+  setTimeout(() => {
+    dog.classList.remove('jump')
+    console.log('class removed')
+    console.log(count)
+  }, 500)
 }
 // To generate the obstacles
 
@@ -23,5 +35,5 @@ const jump = () => {
 ////////////////////////////////
 // Event Listeners Here
 // To initialize/play the game when click/key pressed occurs
-dog.addEventListener('click', jump)
+document.addEventListener('click', jump)
 // For restart button to replay the game if player choose to
